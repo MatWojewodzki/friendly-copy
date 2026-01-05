122fbd12ac7750caa333265f100dd8f1d1ef67a0
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2026-01-05 16:50:32 +0100

    Add and configure prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import "./main.css";
+import './main.css'
 
 function App() {
-  return (
-    <main className="flex justify-center items-center h-screen">
-        <p className="font-bold text-2xl">Hello World!</p>
-    </main>
-  );
+    return (
+        <main className="flex justify-center items-center h-screen">
+            <p className="font-bold text-2xl">Hello World!</p>
+        </main>
+    )
 }
 
-export default App;
+export default App
